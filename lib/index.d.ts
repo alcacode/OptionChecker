4e820a65b549b67761c63ae90b651b021a6242a7
alcacode <alca_addr@protonmail.com>
2020-03-17 23:20:26 +0100

    Delete index.d.ts

--- lib/index.d.ts
+++
@@ -1,5 +0,0 @@
-/// <reference types="src" />
-export declare function parseOptions<O extends OptionList<any>>(optDecl: OptionDeclaration<O>, opts?: {
-    [key: string]: any;
-}): OptionList<O>;
-export declare const OptionChecker: OptionCheckerConstructor;
